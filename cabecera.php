dddc095150ce898638719c59ae35ccbe717a0b5c
ArielMariano <amrosasmartinez@gmail.com>
2021-04-02 00:50:34 -0600

    [ADD] cabecera y estilos

--- cabecera.php
+++ cabecera.php
@@ -6,20 +6,32 @@
     }
 ?>
 <header>
-    <a href = "/">
-        <img alt="logo" src="<?php echo $logo[0] ?>">
-    </a>
-    <?php 
-        wp_nav_menu(
-            array(
-                'theme_location' => 'social-network'
-            )
-        );
-        wp_nav_menu(
-            array(
-                'theme_location' => 'main',
-            )
-        );
-    ?>
+    <div class="left">
+        <a href = "/">
+            <img alt="logo" src="<?php echo $logo[0] ?>">
+        </a>
+    </div>
+    <div class="right">
+        <?php 
+            wp_nav_menu(
+                array(
+                    'theme_location' => 'social-network'
+                )
+            );
+            wp_nav_menu(
+                array(
+                    'theme_location' => 'main',
+                )
+            );
+        ?>
+        <!--movil-->
+        <div class="movil">
+            <span class="dashicons dashicons-menu"></span>
+        </div>
+    </div>
 </header>
 
+<!-- 1._ GENERAR template_part menu movil
+    2._ hacer con html y darle estilos
+    3._ display: none;
+    4._ funcionalidad con javascrit que al presionar se hace display: flex; y cuando no display: none;
